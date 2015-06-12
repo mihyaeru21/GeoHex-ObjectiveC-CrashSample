2ac649fbb9c2a70149a822d6d9d8a880faa8f196
Mihyaeru <mihyaeru21@gmail.com>
2015-06-12 15:20:01 +0900

    implement causing crash code

--- GeoHex-ObjectiveC-CrashSample/ViewController.m
+++ GeoHex-ObjectiveC-CrashSample/ViewController.m
@@ -7,21 +7,33 @@
 //
 
 #import "ViewController.h"
+#import <GeoHexV3.h>
+
 
 @interface ViewController ()
-
+@property (strong, nonatomic, readonly) NSString *code;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //
+    // using manual reference counting
+    //
+
+    _code = [[NSString alloc] initWithUTF8String:"RU0585864"];
+    NSLog(@"retain count: %ld", self.code.retainCount);            // => 1
+
+    GeoHexV3 *geohex = [[GeoHexV3 alloc] initFromCode:self.code];
+    NSLog(@"retain count: %ld", self.code.retainCount);            // => 1
+    NSLog(@"%@", self.code);                                       // => RU0585864
+    NSLog(@"%@", geohex.code);                                     // => RU0585864
+
+    [geohex release];                                              // will release self.code
+
+    NSLog(@"retain count: %ld", self.code.retainCount);            // will cause crash
 }
 
 @end
